b69f98472cc874ad1e8434e2c6f4f53eb032da97
zmffn <zmffn2@gmail.com>
2021-01-08 01:29:23 +0900

    세금

--- Algorithms/src/goorm/no_10.java
+++ Algorithms/src/goorm/no_10.java
@@ -2,7 +2,7 @@ package goorm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+ 
 public class no_10 {
 	public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
