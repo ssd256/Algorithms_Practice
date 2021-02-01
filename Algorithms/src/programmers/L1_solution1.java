365e3fd668ac8ee9524cba2753320f7eb18fb5ec
김미경 <63248384+ssd256@users.noreply.github.com>
2021-02-01 04:29:38 -0900

    L1_solution1

--- Algorithms/src/programmers/L1_solution1.java
+++ Algorithms/src/programmers/L1_solution1.java
@@ -7,13 +7,13 @@ import java.io.InputStreamReader;
 public class L1_solution1 {
 	public static int solution(int n) {
 		int answer = 0;
-		int num = 2;
+		int num = 2; // 소수인지 num에 넣어서 비교
 		
 		while(num <= n) {
 			for(int i = 2; i <= num; i++) {
-				if(num%i == 0 && i != num) {
+				if(num%i == 0 && i != num) { // num이 i로 나눠지면서 서로 같은 값이 아니라면 num은 소수가 아님
 					break;
-				} else if(num%i == 0 && i == num)
+				} else if(num%i == 0 && i == num) // num이 i로 나눠지면서 서로 같은 값이면 num은 소수(자신과 1만 나눠지니까)
 					answer++;
 			}
 			num++;
