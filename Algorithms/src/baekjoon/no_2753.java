fba712a24836bddbda04b7a62984ad0733f09455
ssd256 <zmffn2@gmail.com>
2022-03-23 22:56:09 +0900

    2753번 윤년 구하기

---
+++ Algorithms/src/baekjoon/no_2753.java
@@ -0,0 +1,19 @@
+package Algorithms_Practice.Algorithms.src.baekjoon;
+
+public class no_2753 {
+
+  public static void main(String[] args) {
+    // 윤년 구하기
+    // 4의 배수 && 100의 배수가 아닐 때
+    // 400의 배수일 때
+    Scanner sc = new Scanner(System.in);
+
+    int year = sc.nextInt();
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+      System.out.println("1");
+    } else {
+      System.out.println("0");
+    }
+  }
+}
