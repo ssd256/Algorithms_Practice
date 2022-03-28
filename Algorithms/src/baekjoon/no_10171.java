432cd0c41a8e35a27d7fee20f0d750079e141970
ssd256 <zmffn2@gmail.com>
2022-03-28 22:42:45 +0900

    10171번 고양이 출력

---
+++ Algorithms/src/baekjoon/no_10171.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+public class no_10171 {
+
+  // 고양이 출력하기
+
+  // 한 줄에 최대 9개까지
+  // 총 4줄
+  /*
+    \    /\
+      )  ( ')
+    (  /  )
+      \(__)|
+  */
+
+  public static void main(String[] args) {
+    System.out.println("\\" + "    " + "/\\"); // 역슬래쉬는 \\
+    System.out.println(" " + ")" + "  " + "(" + " " + "')");
+    System.out.println("(" + "  " + "/" + "  " + ")");
+    System.out.println(" " + "\\(__)|");
+  }
+}
